243a8777b9984b593d4d025b2991893fdc76be94
Will Greenberg <willgreenberg@mac.com>
2018-07-18 09:50:04 -0400

    Added comments, cleaned up formatting

--- ebayscraper.py
+++ ebayscraper.py
@@ -11,28 +11,40 @@ from ebaysdk.finding import Connection as Finding
 from ebaysdk.exception import ConnectionError
 import html.parser
 
+
+# Get keywords to search with from user (or hard-coded for testing)
 Keywords = 'pan' #input("Enter a search term: \n")
+
+# Initialize the Finding api from ebaysdk with my appid
 api = Finding(appid='WillGree-Scraper-PRD-d8bba853c-c16e72d4', config_file=None)
+
+# Generate a searchRequest dictionary with all appropriate attributes (consider making this
+# customizable later)
 searchRequest = { 
-		'keywords':Keywords,
-		'outputSelector':'SellerInfo',
-		'paginationInput': [ { 'entriesPerPage':100 } ]
-	}
+	'keywords':Keywords,
+	'outputSelector':'SellerInfo',
+	'paginationInput': [ { 'entriesPerPage':100 } ]
+}
 
+# Execute the search call to eBay
 response = api.execute('findItemsByKeywords', searchRequest)
-soup = soup(response.content, "html.parser")
 
-print(soup.text + "\n")
+# Parse the raw HTML response
+soup = soup(response.content, 'html.parser')
 
-totalentries = int(soup.find('totalentries').text)
-print(str(totalentries) + "\n")
+# DEBUG
+#print(soup.text + "\n")
+#totalentries = int(soup.find('totalentries').text)
+#print(str(totalentries) + "\n")
 
+# Extract all 'items' from the parsed response
 items = soup.find_all('item')
 
-print(str(len(items)) + "\n")
+# DEBUG
+print("Number of items: " + str(len(items)))
 
+# DEBUG - list out item information
 count = 1
-
 for item in items:
-	print(str(count), ": ", item.sellerusername.string + "\n")
+	print(str(count), ": ", item.sellerusername.string)
 	count += 1
